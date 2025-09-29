750e8c4b72a5f0a8029354dd39061eae9a89afd1
Adrián <adrianmirabent@gmail.com>
2025-09-29 13:37:17 +0200

    First update of the first file of the box of the contents.

--- Ejercicio_guiado_1.2.py
+++ Ejercicio_guiado_1.2.py
@@ -1,6 +1,9 @@
 nombre = input("¿Cual es tu nombre?")
 edad = input("¿Cual es tu edad?")
 altura = input("¿Cual es tu altura?")
-estudio = input("¿Que estudias?")
+estudio = input("¿Estudias?")
+
+print("estudio : " + estudio)
+print(bool(estudio))
 
 print(f"Me llamo {nombre}, tengo {edad} y mido {altura} cm y estudio {estudio}")
