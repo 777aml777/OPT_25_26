6308c6cb5d8ff41b9f354b1371b92f4320370df4
Adrián <adrianmirabent@gmail.com>
2025-09-29 13:50:13 +0200

    b1_3_conversor_datos

---
+++ b1_3_conversor_datos.py
@@ -0,0 +1,6 @@
+nombre = input("Introduzca su nombre: ")
+edad = int(input("Introduzca su edad: "))
+altura = float(input("Introduzca su altura en metros: "))
+
+print ()
+print(f"Me llamo {nombre}, tengo {edad} años y mido {altura} metros.")
